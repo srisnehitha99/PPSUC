52ce0a57e44a6fde27812d848997a8ab1dfa4024
srisnehitha99 <nsrisnehitha@gmail.com>
2025-12-03 22:29:28 +0530

    Add files via upload

---
+++ donation reward.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<=3)
+    {printf("BRONZE");}
+    else if(x>3&&x<=6)
+    {printf("SILVER");}
+    else if(x>6)
+    {printf("GOLD");}
+
+}
+
