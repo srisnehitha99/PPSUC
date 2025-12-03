46c14d10da26080cd465ff3e0ee032b6c0ccc4ce
srisnehitha99 <nsrisnehitha@gmail.com>
2025-12-03 21:47:12 +0530

    Add files via upload

---
+++ prime or not prime.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int N, isPrime = 1;
+    scanf("%d", &N);
+    if (N <= 1) {
+        isPrime = 0;
+    } else {
+        for (int i = 2; i * i <= N; i++) {
+            if (N % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+    if (isPrime)
+        printf("Prime");
+    else
+        printf("Not Prime");
+    return 0;
+}
